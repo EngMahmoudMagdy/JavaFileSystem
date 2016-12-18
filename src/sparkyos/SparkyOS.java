c9e06b6cf2f5221249264b64e30a3347dddbf10a
Mahmoud Magdy <Mahmoud Magdy>
2016-12-18 19:43:22 +0200

    Signed-off-by: Mahmoud Magdy <Mahmoud Magdy> it is a file system simulation

---
+++ src/sparkyos/SparkyOS.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sparkyos;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author engma
+ */
+public class SparkyOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Welcome to J-OS");
+        OSFiles.main(args);
+//        TreeIconDemo2.main(args);
+        
+    }
+    
+}
